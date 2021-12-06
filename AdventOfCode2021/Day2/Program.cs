1b68dc50502569f606b9be11537ffba2cf0ccccb
Daan Beusink <daan.beusink@mhsglobal.com>
2021-12-06 20:57:08 +0100

    Day2Part1

--- AdventOfCode2021/Day2/Program.cs
+++ AdventOfCode2021/Day2/Program.cs
@@ -1,1 +1,47 @@
-﻿
+﻿using Day2;
+using System;
+
+Console.WriteLine("Enter submarine movement one-by-one. Stop by typing 'x'.");
+
+var readings = new List<Tuple<SubmarineMovement, int>>();
+bool stop = false;
+while (!stop)
+{
+    var input = Console.ReadLine();
+    if (string.IsNullOrEmpty(input) || input == "x")
+    {
+        stop = true;
+    }
+    else
+    {
+
+        readings.Add(ParseReading(input));
+    }
+}
+
+var navigator = new SubmarineNavigator();
+foreach (var reading in readings)
+{
+    navigator.Move(reading.Item1, reading.Item2);
+}
+
+Console.WriteLine($"Product: {navigator.Product}");
+
+Console.WriteLine("Press any key to continue...");
+Console.ReadKey();
+
+static Tuple<SubmarineMovement, int> ParseReading(ReadOnlySpan<char> input)
+{
+    var spaceIndex = input.IndexOf(' ');
+    var movementString = input.Slice(0, spaceIndex).ToString();
+    var units = input.Slice(spaceIndex + 1);
+    var movement = movementString switch
+    {
+        "forward" => SubmarineMovement.Forward,
+        "up" => SubmarineMovement.Up,
+        "down" => SubmarineMovement.Down,
+        _ => throw new InvalidDataException()
+    };
+
+    return new Tuple<SubmarineMovement, int>(movement, int.Parse(units));
+}
