663a4ba10cc367058b4405b480e1c0907d248332
Daan Beusink <daan.beusink@mhsglobal.com>
2021-12-07 21:54:04 +0100

    Day3Part2

--- AdventOfCode2021/Day3/Program.cs
+++ AdventOfCode2021/Day3/Program.cs
@@ -1,30 +1,24 @@
 ﻿
 
+//var input = await File.ReadAllLinesAsync("input_example.txt");
 var input = await File.ReadAllLinesAsync("input.txt");
-Console.WriteLine(GetPowerConsumption(input, input[0].Length));
+
+// Part 1
+Console.WriteLine($"Power consumption: {GetPowerConsumption(input, input[0].Length)}");
+
+// Part 2
+var oxygenRating = GetAirRating(input, input[0].Length, true);
+var co2ScrubberRating = GetAirRating(input, input[0].Length, false);
+
+Console.WriteLine($"Oxygen rating: {oxygenRating}");
+Console.WriteLine($"C02 scrubber rating: {co2ScrubberRating}");
+Console.WriteLine($"Life support rating: {oxygenRating*co2ScrubberRating}");
 Console.ReadKey();
 
 static int GetPowerConsumption(IEnumerable<string> input, int nrOfUsedBits)
 {
-    int[,] commonBitCounter = new int[nrOfUsedBits, 2];
-    foreach (var reading in input)
-    {
-        int val = Convert.ToInt32(reading, 2);
-        int start = 1 << nrOfUsedBits - 1;
-
-        for (int i = 0; i < nrOfUsedBits; i++)
-        {
-            if ((val & start) == start)
-            {
-                commonBitCounter[i, 1]++;
-            }
-            else
-            {
-                commonBitCounter[i, 0]++;
-            }
-            start >>= 1;
-        }
-    }
+    var numericInput = input.Select(x => Convert.ToInt32(x, 2)).ToArray();
+    var commonBitCounter = GetMostCommonBits(numericInput, nrOfUsedBits);
 
     int gamma = 0;
     for (int i = 0; i < nrOfUsedBits; i++)
@@ -47,4 +41,64 @@ static int GetPowerConsumption(IEnumerable<string> input, int nrOfUsedBits)
     Console.WriteLine($"Epsilon: {epsilon}");
 
     return gamma * epsilon;
+}
+
+static int GetAirRating(IEnumerable<string> input, int nrOfUsedBits, bool considerMostCommon)
+{
+    var numericInput = input.Select(x => Convert.ToInt32(x, 2)).ToArray();
+    var commonBitCounter = GetMostCommonBits(numericInput, nrOfUsedBits);
+
+    var filteredReadings = numericInput;
+    for (int i=0; i<nrOfUsedBits; i++)
+    {
+        var bitMask = 1 << (nrOfUsedBits - 1 - i);
+
+        if (commonBitCounter[i, 1] >= commonBitCounter[i, 0])
+        {
+            filteredReadings = considerMostCommon ?
+                filteredReadings.Where(x => (x & bitMask) == bitMask).ToArray() :
+                filteredReadings.Where(x => (x & bitMask) != bitMask).ToArray();
+        }
+        else
+        {
+            filteredReadings = considerMostCommon ?
+                filteredReadings.Where(x => (x & bitMask) != bitMask).ToArray() :
+                filteredReadings.Where(x => (x & bitMask) == bitMask).ToArray();
+        }
+
+        if (filteredReadings.Length == 1)
+        {
+            return filteredReadings[0];
+        }
+        else
+        {
+            commonBitCounter = GetMostCommonBits(filteredReadings, nrOfUsedBits);
+        }
+    }
+
+    return 0;
+}
+
+static int[,] GetMostCommonBits(IEnumerable<int> input, int nrOfUsedBits)
+{
+    int[,] commonBitCounter = new int[nrOfUsedBits, 2];
+    foreach (var reading in input)
+    {
+        int start = 1 << nrOfUsedBits - 1;
+
+        for (int i = 0; i < nrOfUsedBits; i++)
+        {
+            if ((reading & start) == start)
+            {
+                commonBitCounter[i, 1]++;
+            }
+            else
+            {
+                commonBitCounter[i, 0]++;
+            }
+            start >>= 1;
+        }
+    }
+
+    return commonBitCounter;
 }
